55c867b4860f65801359665221ce8896863b0940
eutro <benedek.szilvasy@gmail.com>
2022-09-05 13:39:08 +0200

    Implement all the intrinsics

--- src/main/java/io/github/eutro/wasm2j/intrinsics/Intrinsic.java
+++ src/main/java/io/github/eutro/wasm2j/intrinsics/Intrinsic.java
@@ -2,14 +2,17 @@ package io.github.eutro.wasm2j.intrinsics;
 
 import io.github.eutro.jwasm.Opcodes;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.CLASS)
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Intrinsic {
     byte value() default Opcodes.INSN_PREFIX;
 
-    int iOp() default 0;
+    int iOp() default -1;
 
     boolean inline() default true;
 }
--- src/test/java/io/github/eutro/wasm2j/test/IntrinsicsTest.java
+++ src/test/java/io/github/eutro/wasm2j/test/IntrinsicsTest.java
@@ -1,6 +1,8 @@
 package io.github.eutro.wasm2j.test;
 
 import io.github.eutro.wasm2j.ext.JavaExts;
+import io.github.eutro.wasm2j.intrinsics.Impl;
+import io.github.eutro.wasm2j.intrinsics.Intrinsic;
 import io.github.eutro.wasm2j.intrinsics.IntrinsicImpl;
 import io.github.eutro.wasm2j.intrinsics.JavaIntrinsics;
 import io.github.eutro.wasm2j.passes.*;
@@ -26,8 +28,17 @@ import org.objectweb.asm.tree.MethodNode;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class IntrinsicsTest {
+
+    public static final int LOOP_COUNT = 20_000;
+
     @Test
     void testIntrinsicsJir() {
         int i = 0;
@@ -90,5 +101,60 @@ public class IntrinsicsTest {
                 os.write(classBytes);
             }
         }
+    }
+
+    @Test
+    void testIntrinsicsAssembly() throws ReflectiveOperationException {
+        MethodHandles.Lookup lookup = MethodHandles.lookup();
+        Map<Class<?>, MethodHandle> rngs = new HashMap<>();
+
+        Random rand = new Random();
+        rngs.put(int.class, lookup
+                .findVirtual(Random.class, "nextInt", MethodType.methodType(int.class))
+                .bindTo(rand));
+        rngs.put(long.class, lookup
+                .findVirtual(Random.class, "nextLong", MethodType.methodType(long.class))
+                .bindTo(rand));
+        rngs.put(float.class, lookup
+                .findVirtual(Random.class, "nextFloat", MethodType.methodType(float.class))
+                .bindTo(rand));
+        rngs.put(double.class, lookup
+                .findVirtual(Random.class, "nextDouble", MethodType.methodType(double.class))
+                .bindTo(rand));
+
+        List<MethodHandle> invokers = new ArrayList<>();
+        for (Method method : Impl.class.getMethods()) {
+            if (method.getAnnotation(Intrinsic.class) == null) continue;
+
+            MethodHandle handle = lookup.unreflect(method);
+
+            for (Class<?> pType : method.getParameterTypes()) {
+                MethodHandle rng = rngs.get(pType);
+                if (rng == null) {
+                    throw new IllegalStateException();
+                }
+                handle = MethodHandles.collectArguments(handle, 0, rng);
+            }
+
+            invokers.add(handle);
+        }
+
+        List<List<Object>> allResults = new ArrayList<>();
+        for (MethodHandle invoker : invokers) {
+            Object[] results = new Object[LOOP_COUNT];
+            for (int i = 0; i < LOOP_COUNT; i++) {
+                try {
+                    results[i] = invoker.invoke();
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                }
+            }
+            allResults.add(Arrays.asList(results));
+        }
+        // System.out.println(allResults);
+    }
+
+    public static void main(String[] args) throws Throwable {
+        new IntrinsicsTest().testIntrinsicsAssembly();
     }
 }
